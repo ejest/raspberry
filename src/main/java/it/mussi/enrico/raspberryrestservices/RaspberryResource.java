54a9196e271585a31cab09b097c41411efa21462
Enrico <enrico.mussi@gmail.com>
2018-08-15 10:42:07 +0200

    initial project version

---
+++ src/main/java/it/mussi/enrico/raspberryrestservices/RaspberryResource.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.mussi.enrico.raspberryrestservices;
+
+/**
+ *
+ * @author enrico
+ */
+public class RaspberryResource {
+    
+}
